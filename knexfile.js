d320172c8f220125fbb5e3841f16d9f0121127bd
RootedWorks Labs <corey.sanders@rootedworkslabs.com>
2025-08-14 21:20:58 -0400

    updated CI/CD pipeline for deployment

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,11 @@ module.exports = {
   development: {
     client: 'pg',  // Uses your existing 'pg' module
     connection: {
-      host: 'localhost',
-      user: 'postgres',  // Replace with your PostgreSQL username
-      password: '$@nd3r$C0',  // Replace with your PostgreSQL password
-      database: 'vibespace',  // Your database name
-      port: 5432  // Default PostgreSQL port
+      host: process.env.PG_HOST,
+      user: process.env.PG_USER,  // Replace with your PostgreSQL username
+      password: process.env.PG_PASSWORD,  // Replace with your PostgreSQL password
+      database: process.env.PG_DATABASE,  // Your database name
+      port: process.env.PG_PORT,  // Default PostgreSQL port
     },
     migrations: {
       tableName: 'knex_migrations',  // Tracks which migrations have been run
